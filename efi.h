67fbba58fe876c3c0c7db043e3ffd406112bfc75
Nicu <nicuborta@tutanota.com>
2022-11-18 19:36:07 +0200

    Add graphics

--- efi.h
+++ efi.h
@@ -6,6 +6,7 @@
 
 #define EFI_SUCCESS	0
 
+typedef unsigned char		UINT8;
 typedef unsigned short		UINT16;
 typedef int			INT32;
 typedef unsigned int		UINT32;
@@ -13,12 +14,13 @@ typedef unsigned long long	UINT64;
 typedef UINT64			UINTN;
 typedef UINT16			CHAR16;
 typedef unsigned char		BOOLEAN;
+typedef UINT64			EFI_PHYSICAL_ADDRESS;
 typedef void *			EFI_HANDLE;
 typedef UINTN			EFI_STATUS;
 typedef void *			EFI_EVENT;
 
 
-typedef struct EFI_INPUT_KEY {
+typedef struct {
 	UINT16	ScanCode;
 	CHAR16	UnicodeChar;
 } EFI_INPUT_KEY;
@@ -34,7 +36,7 @@ typedef EFI_STATUS (*EFI_INPUT_READ_KEY)(
 		struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL *This, EFI_INPUT_KEY *Key);
 
 // UEFI 2.10 Specs PDF Page 395
-typedef struct EFI_SIMPLE_TEXT_INPUT_PROTOCOL {
+typedef struct {
 	EFI_INPUT_RESET		Reset;
 	EFI_INPUT_READ_KEY	ReadKeyStroke;
 	EFI_EVENT		WaitForKey;
@@ -75,7 +77,7 @@ typedef EFI_STATUS (*EFI_TEXT_ENABLE_CURSOR)(
 		struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *This, BOOLEAN Visible);
 
 // UEFI 2.10 Specs PDF Page 399
-typedef struct SIMPLE_TEXT_OUTPUT_MODE {
+typedef struct {
 	INT32		MaxMode;
 	// current settings
 	INT32		Mode;
@@ -96,15 +98,91 @@ typedef struct EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL {
 	EFI_TEXT_CLEAR_SCREEN		ClearScreen;
 	EFI_TEXT_SET_CURSOR_POSITION	SetCursorPosition;
 	EFI_TEXT_ENABLE_CURSOR		EnableCursor;
-	SIMPLE_TEXT_OUTPUT_MODE		*Mode;
+	SIMPLE_TEXT_OUTPUT_MODE *	Mode;
 } EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL;
+
+
+typedef enum {
+	PixelRedGreenBlueReserved8BitPerColor,
+	PixelBlueGreenRedReserved8BitPerColor,
+	PixelBitMask,
+	PixelBltOnly,
+	PixelFormatMax
+} EFI_GRAPHICS_PIXEL_FORMAT;
+
+typedef struct {
+	UINT32	RedMask;
+	UINT32	GreenMask;
+	UINT32	BlueMask;
+	UINT32	ReservedMask;
+} EFI_PIXEL_BITMASK;
+
+typedef struct {
+	UINT32				Version;
+	UINT32				HorizontalResolution;
+	UINT32				VerticalResolution;
+	EFI_GRAPHICS_PIXEL_FORMAT	PixelFormat;
+	EFI_PIXEL_BITMASK		PixelInformation;
+	UINT32				PixelsPerScanLine;
+} EFI_GRAPHICS_OUTPUT_MODE_INFORMATION;
+
+typedef struct {
+	UINT8	Blue;
+	UINT8	Green;
+	UINT8	Red;
+	UINT8	Reserved;
+} EFI_GRAPHICS_OUTPUT_BLT_PIXEL;
+
+typedef enum {
+	EfiBltVideoFill,
+	EfiBltVideoToBltBuffer,
+	EfiBltBufferToVideo,
+	EfiBltVideoToVideo,
+	EfiGraphicsOutputBltOperationMax
+} EFI_GRAPHICS_OUTPUT_BLT_OPERATION;
+
+typedef struct {
+	UINT32					MaxMode;
+	UINT32					Mode;
+	EFI_GRAPHICS_OUTPUT_MODE_INFORMATION *	Info;
+	UINTN					SizeOfInfo;
+	EFI_PHYSICAL_ADDRESS			FrameBufferBase;
+	UINTN					FrameBufferSize;
+} EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE;
+
+// We are forward declaring this struct so that the function typedefs can operate.
+struct EFI_GRAPHICS_OUTPUT_PROTOCOL;
+
+typedef EFI_STATUS (*EFI_GRAPHICS_OUTPUT_PROTOCOL_QUERY_MODE)(
+		struct EFI_GRAPHICS_OUTPUT_PROTOCOL *This,
+		UINT32 ModeNumber, UINTN *SizeOfInfo,
+		EFI_GRAPHICS_OUTPUT_MODE_INFORMATION **Info);
+
+typedef EFI_STATUS (*EFI_GRAPHICS_OUTPUT_PROTOCOL_SET_MODE)(
+		struct EFI_GRAPHICS_OUTPUT_PROTOCOL *This, UINT32 ModeNumber);
+
+typedef EFI_STATUS (*EFI_GRAPHICS_OUTPUT_PROTOCOL_BLT)(
+		struct EFI_GRAPHICS_OUTPUT_PROTOCOL *This,
+		EFI_GRAPHICS_OUTPUT_BLT_PIXEL *BltBuffer,
+		EFI_GRAPHICS_OUTPUT_BLT_OPERATION BltOperation,
+		UINTN SourceX, UINTN SourceY,
+		UINTN DestinationX, UINTN DestinationY,
+		UINTN Width, UINTN Height, UINTN Delta);
+
+// UEFI 2.10 Specs PDF Page 427
+typedef struct EFI_GRAPHICS_OUTPUT_PROTOCOL {
+	EFI_GRAPHICS_OUTPUT_PROTOCOL_QUERY_MODE		QueryMode;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL_SET_MODE		SetMode;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL_BLT		Blt;
+	EFI_GRAPHICS_OUTPUT_PROTOCOL_MODE *		Mode;
+} EFI_GRAPHICS_OUTPUT_PROTCOL;
 
 
 /*
  * This is the main EFI header for all of the EFI protocols.
  * UEFI 2.10 Specs PDF Page 89
  */
-typedef struct EFI_TABLE_HEADER {
+typedef struct {
 	UINT64	Signature;
 	UINT32	Revision;
 	UINT32	HeaderSize;
@@ -117,14 +195,14 @@ typedef struct EFI_TABLE_HEADER {
  * called from the main section.
  * UEFI 2.10 Specs PDF Page 90
  */
-typedef struct EFI_SYSTEM_TABLE {
+typedef struct {
 	EFI_TABLE_HEADER			hrd;
-	CHAR16					*FirmwareVendor;
+	CHAR16 *				FirmwareVendor;
 	UINT32					FirmwareVersion;
 	EFI_HANDLE				ConsoleInHandle;
-	EFI_SIMPLE_TEXT_INPUT_PROTOCOL		*ConIn;
+	EFI_SIMPLE_TEXT_INPUT_PROTOCOL *	ConIn;
 	EFI_HANDLE				ConsoleOutHandle;
-	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL		*ConOut;
+	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *	ConOut;
 } EFI_SYSTEM_TABLE;
 
 #endif // EFI_H
