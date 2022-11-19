52bfaef1f6f01978159aeecdb275a22dc7ee883e
Nicu <nicuborta@tutanota.com>
2022-11-19 15:13:02 +0200

    Continue implementation of system table

--- efi.h
+++ efi.h
@@ -23,6 +23,18 @@ typedef void *			EFI_EVENT;
 
 #define EFI_ERROR(Status) (((INTN)Status) < 0)
 
+/*
+ * This is the main EFI header for all of the EFI protocols.
+ * UEFI 2.10 Specs PDF Page 89
+ */
+typedef struct {
+	UINT64	Signature;
+	UINT32	Revision;
+	UINT32	HeaderSize;
+	UINT32	CRC32;
+	UINT32	Reserved;
+} EFI_TABLE_HEADER;
+
 
 typedef struct {
 	UINT16	ScanCode;
@@ -182,31 +194,81 @@ typedef struct EFI_GRAPHICS_OUTPUT_PROTOCOL {
 } EFI_GRAPHICS_OUTPUT_PROTCOL;
 
 
-/*
- * This is the main EFI header for all of the EFI protocols.
- * UEFI 2.10 Specs PDF Page 89
- */
 typedef struct {
-	UINT64	Signature;
-	UINT32	Revision;
-	UINT32	HeaderSize;
-	UINT32	CRC32;
-	UINT32	Reserved;
-} EFI_TABLE_HEADER;
+	UINT16	Year;
+	UINT8	Month;
+	UINT8	Day;
+	UINT8	Hour;
+	UINT8	Minute;
+	UINT8	Second;
+	UINT8	Pad1;
+	UINT32	Nanosecond;
+	INT16	TimeZone
+	UINT8	Daylight;
+	UINT8	Pad2;
+} EFI_TIME;
+
+typedef struct {
+	UINT32		Resolution;
+	UINT32		Accuracy;
+	BOOLEAN		SetsToZero;
+} EFI_TIME_CAPABILITIES;
+
+/*
+ * NOTE: Time functions are wrongly defined in the spec, e.g. function name is
+ * SetTime instead of EFI_SET_TIME.
+ */
+typedef EFI_STATUS (*EFI_GET_TIME)(EFI_TIME *Time,
+	EFI_TIME_CAPABILITIES *Capabilities);
+
+typedef EFI_STATUS (*EFI_SET_TIME)(EFI_TIME *Time);
+
+typedef EFI_STATUS (*EFI_GET_WAKEUP_TIME)(BOOLEAN *Enabled, BOOLEAN *Pending,
+	EFI_TIME *Time);
+
+typedef EFI_STATUS (*EFI_SET_WAKEUP_TIME)(BOOLEAN Enable, EFI_TIME *Time);
+
+// TODO: EFI_RUNTIME_SERVICES members from SET_VIRTUAL_ADDRESS_MAP
+
+// UEFI 2.10 Specs PDF Page 96
+typedef struct {
+	EFI_TABLE_HEADER		Hdr;
+	EFI_GET_TIME			GetTime;
+	EFI_SET_TIME			SetTime;
+	EFI_GET_WAKEUP_TIME		GetWakeupTime;
+	EFI_SET_WAKEUP_TIME		SetWakeupTime;
+	EFI_SET_VIRTUAL_ADDRESS_MAP	SetVirtualAddressMap;
+	EFI_CONVERT_POINTER		ConvertPointer;
+	EFI_GET_VARIABLE		GetVariable;
+	EFI_GET_NEXT_VARIABLE_NAME	GetNextVariableName;
+	EFI_SET_VARIABLE		SetVariable;
+	EFI_GET_NEXT_HIGH_MONO_COUNT	GetNextHighMonotonicCount;
+	EFI_RESET_SYSTEM		ResetSystem;
+	EFI_UPDATE_CAPSULE		UpdateCapsule;
+	EFI_QUERY_CAPSULE_CAPABILITIES	QueryCapsuleCapabilities;
+	EFI_QUERY_VARIABLE_INFO		QueryVariableInfo;
+} EFI_RUNTIME_SERVICES;
+
 
 /*
  * EFI has a system and runtime. This system table is the first struct
  * called from the main section.
- * UEFI 2.10 Specs PDF Page 90
+ * UEFI 2.10 Specs PDF Page 91
  */
 typedef struct {
-	EFI_TABLE_HEADER			hrd;
+	EFI_TABLE_HEADER			Hdr;
 	CHAR16 *				FirmwareVendor;
 	UINT32					FirmwareVersion;
 	EFI_HANDLE				ConsoleInHandle;
 	EFI_SIMPLE_TEXT_INPUT_PROTOCOL *	ConIn;
 	EFI_HANDLE				ConsoleOutHandle;
 	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *	ConOut;
+	EFI_HANDLE				StandardErrorHandle;
+	EFI_SIMPLE_TEXT_OUTPUT_PROTOCOL *	StdErr;
+	EFI_RUNTIME_SERVICES *			RuntimeServices;
+	EFI_BOOT_SERVICES *			BootServices;
+	UINTN					NumberOfTableEntries;
+	EFI_CONFIGURATION_TABLE *		ConfigurationTable;
 } EFI_SYSTEM_TABLE;
 
 #endif // EFI_H
